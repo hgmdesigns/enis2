1dc044e4a8a0b4bbe570fdd8a95a0f3fcb52f59e
prince-73ssan <prince532@outlook.com>
2017-08-27 00:24:32 +0400

    Calendar with lists
    In the perivous version there was no data renderd for dates, In this version it is fixed!

--- App.js
+++ App.js
@@ -30,56 +30,92 @@ class CalenderScreen extends React.Component{
   static navigationOptions = {
     title: 'CALENDER',
   };
-  render(){
-    return(
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: {}
+    };
+  }
+
+  render() {
+    return (
       <Agenda
-      // the list of items that have to be displayed in agenda. If you want to render item as empty date
-      // the value of date key kas to be an empty array []. If there exists no value for date key it is
-      // considered that the date in question is not yet loaded
-      items={
-        {'2017-08-31': [{text: 'item 1 - any js object'}],
-         '2017-08-30': [{text: 'item 2 - any js object'}],
-         '2017-08-29': [],
-         '2017-08-28': [{text: 'item 3 - any js object'},{text: 'any js object'}],
-        }}
-      // callback that gets called when items for a certain month should be loaded (month became visible)
-      loadItemsForMonth={(month) => {console.log('trigger items loading')}}
-      // callback that gets called on day press
-      onDayPress={(day)=>{console.log('day pressed')}}
-      // callback that gets called when day changes while scrolling agenda list
-      onDayChange={(day)=>{console.log('day changed')}}
-      // initially selected day
-      selected={'2017-08-26'}
-      // Minimum date that can be selected, dates before minDate will be grayed out. Default = undefined
-      minDate={'2012-05-10'}
-      // Maximum date that can be selected, dates after maxDate will be grayed out. Default = undefined
-      maxDate={'2012-05-30'}
-      // specify how each item should be rendered in agenda
-      renderItem={(item, firstItemInDay) => {return (<View />);}}
-      // specify how each date should be rendered. day can be undefined if the item is not first in that day.
-      renderDay={(day, item) => {return (<View />);}}
-      // specify how empty date content with no items should be rendered
-      renderEmptyDate={() => {return (<View />);}}
-      // specify how agenda knob should look like
-      renderKnob={() => {return (<View />);}}
-      // specify your item comparison function for increased performance
-      rowHasChanged={(r1, r2) => {return r1.text !== r2.text}}
-      // Hide knob button. Default = false
-      hideKnob={true}
-      // agenda theme
-      // theme={{
-      //   ...calendarTheme,
-      //   agendaDayTextColor: 'yellow',
-      //   agendaDayNumColor: 'green',
-      //   agendaTodayColor: 'red',
-      //   agendaKnobColor: 'blue'
-      // }}
-      // agenda container style
-      style={{}}
-  />
-    )
+        items={this.state.items}
+        loadItemsForMonth={this.loadItems.bind(this)}
+        selected={'2017-08-26'}
+        renderItem={this.renderItem.bind(this)}
+        renderEmptyDate={this.renderEmptyDate.bind(this)}
+        rowHasChanged={this.rowHasChanged.bind(this)}
+        // monthFormat={'yyyy'}
+        // theme={{calendarBackground: 'red', agendaKnobColor: 'green'}}
+        //renderDay={(day, item) => (<Text>{day ? day.day: 'item'}</Text>)}
+      />
+    );
+  }
+
+  loadItems(day) {
+    setTimeout(() => {
+      for (let i = -15; i < 85; i++) {
+        const time = day.timestamp + i * 24 * 60 * 60 * 1000;
+        const strTime = this.timeToString(time);
+        if (!this.state.items[strTime]) {
+          this.state.items[strTime] = [];
+          const numItems = Math.floor(Math.random() * 5);
+          for (let j = 0; j < numItems; j++) {
+            this.state.items[strTime].push({
+              name: 'SS Test on ' + strTime,
+              height: Math.max(50, Math.floor(Math.random() * 150))
+            });
+          }
+        }
+      }
+      //console.log(this.state.items);
+      const newItems = {};
+      Object.keys(this.state.items).forEach(key => {newItems[key] = this.state.items[key];});
+      this.setState({
+        items: newItems
+      });
+    }, 1000);
+    // console.log(`Load Items for ${day.year}-${day.month}`);
+  }
+
+  renderItem(item) {
+    return (
+      <View style={[styles.item, {height: item.height}]}><Text>{item.name}</Text></View>
+    );
+  }
+
+  renderEmptyDate() {
+    return (
+      <View style={styles.emptyDate}><Text>This is empty date!</Text></View>
+    );
+  }
+
+  rowHasChanged(r1, r2) {
+    return r1.name !== r2.name;
+  }
+
+  timeToString(time) {
+    const date = new Date(time);
+    return date.toISOString().split('T')[0];
   }
 }
+
+const styles = StyleSheet.create({
+  item: {
+    backgroundColor: 'white',
+    flex: 1,
+    borderRadius: 5,
+    padding: 10,
+    marginRight: 10,
+    marginTop: 17
+  },
+  emptyDate: {
+    height: 15,
+    flex:1,
+    paddingTop: 30
+  }
+});
 
 class PaperScreen extends React.Component{
   static navigationOptions = {
