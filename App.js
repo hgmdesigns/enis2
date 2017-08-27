ac29cfdbe30d010c90b319f03008cacaf2ff88da
prince-73ssan <prince532@outlook.com>
2017-08-27 20:10:33 +0400

    Throwing error
    NOt useful build

--- App.js
+++ App.js
@@ -33,9 +33,8 @@ class CalenderScreen extends React.Component{
   constructor(props) {
     super(props);
     this.state = {
-      items: {}
-    };
-  }
+      items: {} }
+}
 
   render() {
     return (
@@ -54,29 +53,10 @@ class CalenderScreen extends React.Component{
   }
 
   loadItems(day) {
-    setTimeout(() => {
-      for (let i = -15; i < 85; i++) {
-        const time = day.timestamp + i * 24 * 60 * 60 * 1000;
-        const strTime = this.timeToString(time);
-        if (!this.state.items[strTime]) {
-          this.state.items[strTime] = [];
-          const numItems = Math.floor(Math.random() * 5);
-          for (let j = 0; j < numItems; j++) {
-            this.state.items[strTime].push({
-              name: 'SS Test on ' + strTime,
-              height: Math.max(50, Math.floor(Math.random() * 150))
-            });
-          }
-        }
-      }
-      //console.log(this.state.items);
-      const newItems = {};
-      Object.keys(this.state.items).forEach(key => {newItems[key] = this.state.items[key];});
-      this.setState({
-        items: newItems
-      });
-    }, 1000);
-    // console.log(`Load Items for ${day.year}-${day.month}`);
+    item: {
+      text: "test",
+      height: 86
+    }
   }
 
   renderItem(item) {
